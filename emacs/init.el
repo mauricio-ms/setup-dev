2bb7a0d8e691940bf7a281848a76ea33e526e3b5
Maurício Mussatto Scopel <ms.mauricio93@gmail.com>
2023-07-21 14:41:46 -0300

    emacs: keeping folders clean

--- emacs/init.el
+++ emacs/init.el
@@ -23,6 +23,12 @@
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
+;; Teaching emacsto keep your folders clean
+(setq backup-directory-alist `(("." . ,(expand-file-name "tmp/backups/" user-emacs-directory))))
+(make-directory (expand-file-name "tmp/auto-saves/" user-emacs-directory) t)
+(setq auto-save-list-file-prefix (expand-file-name "tmp/auto-saves/sessions/" user-emacs-directory)
+      auto-save-file-name-transforms `((".*" ,(expand-file-name "tmp/auto-saves/" user-emacs-directory) t)))
+
 ;; Initialize package sources
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
