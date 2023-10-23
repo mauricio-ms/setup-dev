da8364a6a8e1c1b46c14cefb271aeb81ed58a0ab
Maurício Mussatto Scopel <ms.mauricio93@gmail.com>
2023-10-22 21:36:27 -0300

    emacs: add org-roam

--- emacs/init.el
+++ emacs/init.el
@@ -485,6 +485,30 @@
 	  (org-babel-tangle))))
 (add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'efs/org-babel-tangle-config)))
 
+;; org roam
+(use-package org-roam
+  :custom
+  (org-roam-directory (file-truename "~/development/notebook/roam-notes"))
+  (org-roam-completion-everywhere t)
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+		 ("C-c n f" . org-roam-node-find)
+		 ("C-c n i" . org-roam-node-insert)
+		 :map org-mode-map
+		 ("C-M-i" . completion-at-point)
+		 :map org-roam-dailies-map
+		 ("Y" . org-roam-dailies-capture-yesterday)
+		 ("T" . org-roam-dailies-capture-tomorrow))
+  :bind-keymap
+  ("C-c n d" . org-roam-dailies-map)
+  :config
+  (require 'org-roam-dailies) ;; Ensure the keymap is available
+  (org-roam-setup))
+
+;; drag and drop images to org-mode buffers
+(use-package org-download)
+;; Drag-and-drop to `dired`
+(add-hook 'dired-mode-hook 'org-download-enable)
+
 (use-package evil-nerd-commenter
   :bind ("M-/" . evilnc-comment-or-uncomment-lines))
 
