3ab1275f611b4ca201d1bf03fa9c4c83ea783e71
Maurício Mussatto Scopel <ms.mauricio93@gmail.com>
2023-11-02 13:54:08 -0300

    emacs: add org-roam

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,4 @@
+;; -*- lexical-binding: t; -*-
 (setq inhibit-startup-message t)
 
 (scroll-bar-mode -1)    ; Disable visible scrollbar
@@ -247,8 +248,118 @@
 
 (use-package forge) ;; TODO The GitHub authentication needs to be configure, check at magit documentation page
 
-;; org mode
+;; org roam
+(use-package org-roam
+  :demand t
+  :custom
+  (org-roam-directory (file-truename "~/development/notebook/roam-notes"))
+  (org-roam-completion-everywhere t)
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+		 ("C-c n f" . org-roam-node-find)
+		 ("C-c n i" . org-roam-node-insert)
+		 ("C-c n I" . org-roam-node-insert-immediate)
+		 ("C-c n p" . setup-dev/org-roam-find-project)
+		 ("C-c n t" . setup-dev/org-roam-capture-task)
+		 :map org-mode-map
+		 ("C-M-i" . completion-at-point)
+		 :map org-roam-dailies-map
+		 ("Y" . org-roam-dailies-capture-yesterday)
+		 ("T" . org-roam-dailies-capture-tomorrow))
+  :bind-keymap
+  ("C-c n d" . org-roam-dailies-map)
+  :config
+  (require 'org-roam-dailies) ;; Ensure the keymap is available
+  (org-roam-setup))
 
+(defun org-roam-node-insert-immediate (arg &rest args)
+  (interactive "P")
+  (let ((args (push arg args))
+        (org-roam-capture-templates (list (append (car org-roam-capture-templates)
+                                                  '(:immediate-finish t)))))
+    (apply #'org-roam-node-insert args)))
+
+(defun setup-dev/org-roam-filter-by-tag (tag-name)
+  (lambda (node)
+    (member tag-name (org-roam-node-tags node))))
+
+(defun setup-dev/org-roam-list-notes-by-tag (tag-name)
+  (mapcar #'org-roam-node-file
+         (seq-filter
+           (setup-dev/org-roam-filter-by-tag tag-name)
+           (org-roam-node-list))))
+
+(defun setup-dev/org-roam-refresh-agenda-list ()
+  (interactive)
+  (setq org-agenda-files (setup-dev/org-roam-list-notes-by-tag "Project")))
+
+;; Build the agenda list the first time for the session
+(setup-dev/org-roam-refresh-agenda-list)
+
+(defun setup-dev/org-roam-project-finalize-hook ()
+  "Adds the captured project file to `org-agenda-files' if the capture was not aborted."
+  ;; Remove the hook since it was added temporarily
+  (remove-hook 'org-capture-after-finalize-hook #'setup-dev/org-roam-project-finalize-hook)
+
+  ;; Add project file to the agenda list if the capture was confirmed
+  (unless org-note-abort
+    (with-current-buffer (org-capture-get :buffer)
+      (add-to-list 'org-agenda-files (buffer-file-name)))))
+
+(defun setup-dev/org-roam-find-project ()
+  (interactive)
+  ;; Add the project file to the agenda after capture is finished
+  (add-hook 'org-capture-after-finalize-hook #'setup-dev/org-roam-project-finalize-hook)
+
+  ;; Select a project file to open, creating it if necessary
+  (org-roam-node-find
+   nil
+   nil
+   (setup-dev/org-roam-filter-by-tag "Project")
+   nil
+   :templates
+   '(("p" "project" plain "* Goals\n\n%?\n\n* Tasks\n\n** TODO Add initial tasks\n\n* Dates\n\n"
+      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+category: ${title}\n#+filetags: Project")
+      :unnarrowed t))))
+
+(defun setup-dev/org-roam-capture-task ()
+  (interactive)
+  ;; Add the project file to the agenda after capture is finished
+  (add-hook 'org-capture-after-finalize-hook #'setup-dev/org-roam-project-finalize-hook)
+
+  ;; Capture the new task, creating the project file if necessary
+  (org-roam-capture- :node (org-roam-node-read
+                            nil
+                            (setup-dev/org-roam-filter-by-tag "Project"))
+                     :templates '(("p" "project" plain "** TODO %?"
+                                   :if-new (file+head+olp "%<%Y%m%d%H%M%S>-${slug}.org"
+                                                          "#+title: ${title}\n#+category: ${title}\n#+filetags: Project"
+                                                          ("Tasks"))))))
+
+(defun setup-dev/org-roam-copy-todo-to-today ()
+  (interactive)
+  (let ((org-refile-keep t) ;; Set this to nil to delete the original!
+        (org-roam-dailies-capture-templates
+          '(("t" "tasks" entry "%?"
+             :if-new (file+head+olp "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n" ("Tasks")))))
+        (org-after-refile-insert-hook #'save-buffer)
+        today-file
+        pos)
+    (save-window-excursion
+      (org-roam-dailies--capture (current-time) t)
+      (setq today-file (buffer-file-name))
+      (setq pos (point)))
+
+    ;; Only refile if the target file is different than the current file
+    (unless (equal (file-truename today-file)
+                   (file-truename (buffer-file-name)))
+      (org-refile nil nil (list "Tasks" today-file nil pos)))))
+
+(add-to-list 'org-after-todo-state-change-hook
+             (lambda ()
+               (when (equal org-state "DONE")
+                 (setup-dev/org-roam-copy-todo-to-today))))
+
+;; org mode
 ;; Load org-faces to make sure we can set appropriate faces
 (require 'org-faces)
 
@@ -300,10 +411,6 @@
   (setq org-agenda-start-with-log-mode t)
   (setq org-log-done 'time)
   (setq org-log-into-drawer t)
-
-  (setq org-agenda-files
-	'("~/development/org-files/tasks.org"
-	  "~/development/org-files/archive.org"))
 
   (require 'org-habit)
   (add-to-list 'org-modules 'org-habit)
@@ -476,33 +583,16 @@
 (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
 (add-to-list 'org-structure-template-alist '("cpp" . "src cpp"))
 
-;; Automatically tangle README.org file from C++ book when it is saved
+;; Automatically tangle my README.org files when them are saved
 (defun efs/org-babel-tangle-config ()
-  (when (string-equal (buffer-file-name)
-					  (expand-file-name "~/development/books/programming-principles-and-practice-using-c++/README.org"))
+  (when (or (string-equal (buffer-file-name)
+						  (expand-file-name "~/development/books/programming-principles-and-practice-using-c++/README.org"))
+			(string-equal (buffer-file-name)
+						  (expand-file-name "~/development/books/compilers-dragon-book/README.org")))
 	;; Dynamic scoping to the rescue
 	(let ((org-confirm-babel-evaluate nil))
 	  (org-babel-tangle))))
 (add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'efs/org-babel-tangle-config)))
-
-;; org roam
-(use-package org-roam
-  :custom
-  (org-roam-directory (file-truename "~/development/notebook/roam-notes"))
-  (org-roam-completion-everywhere t)
-  :bind (("C-c n l" . org-roam-buffer-toggle)
-		 ("C-c n f" . org-roam-node-find)
-		 ("C-c n i" . org-roam-node-insert)
-		 :map org-mode-map
-		 ("C-M-i" . completion-at-point)
-		 :map org-roam-dailies-map
-		 ("Y" . org-roam-dailies-capture-yesterday)
-		 ("T" . org-roam-dailies-capture-tomorrow))
-  :bind-keymap
-  ("C-c n d" . org-roam-dailies-map)
-  :config
-  (require 'org-roam-dailies) ;; Ensure the keymap is available
-  (org-roam-setup))
 
 ;; drag and drop images to org-mode buffers
 (use-package org-download)
